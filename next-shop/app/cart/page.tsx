8e81bf61c945b565b085208b71151495f1d81d49
yessinerah <ahmed.rahmeni2016@gmail.com>
2024-02-22 13:07:40 +0100

    cart without the add functionality

--- next-shop/app/cart/page.tsx
+++ next-shop/app/cart/page.tsx
@@ -1,79 +1,108 @@
-'use client'
+"use client"
 import React from 'react';
 
 interface Product {
-  id: number;
-  imageUrl: string;
+  idproduct: number;
   name: string;
   description: string;
   initalprice: number;
   category: string;
+  imgproducts: { image: string }[];
+  quantity: number;
 }
 
 export default function Cart() {
   const [products, setProducts] = React.useState<Product[]>([]);
-  const [quantity, setQuantity] = React.useState<number>(1);
+console.log(products);
 
   React.useEffect(() => {
     async function fetchData() {
       const response = await fetch(`http://localhost:3001/panier/getAllcarts/1`);
       const data = await response.json();
-      setProducts(data);
+      setProducts(data.map((product: Product) => ({ ...product, quantity: 1 })));
     }
 
     fetchData();
   }, []);
 
-  console.log(products);
+  function calculateSubtotal(product: Product): number {
+    return product.initalprice * product.quantity;
+  }
+
+  const handleDelete = (productIdproduct: number) => {
+    fetch(`http://localhost:3001/panier/deletecart/1/${productIdproduct}`, {
+      method: 'DELETE',
+    }).then((response) => {
+      
+      if (!response.ok) throw Error('Failed to delete');
+      setProducts((products) => products.filter((product) => product.idproduct !== productIdproduct));
+    }).catch((error) => console.log(error));
+  };
 
   return (
-    <div className="max-w-6xl mx-auto p-8  rounded-md shadow-md flex flex-col">
-      <br />
-      <br />
-      <h2 className="text-3xl font-bold mb-6 ">Your Cart</h2>
-      <br />
-      <br />
-      <br />
+    <div className="max-w-6xl mx-auto p-8 rounded-md shadow-md flex flex-col">
+      <h2 className="text-3xl font-bold mb-6">Your Cart</h2>
 
       <div className="overflow-x-auto">
         <table className="w-full table-auto">
           <thead>
             <tr>
-              <th className="px-6 py-10 text-left text-lg font-semibold text-gray-800">Product</th>
+              <th className="px-6 py-3 text-left text-lg font-semibold text-gray-800">Product</th>
               <th className="px-6 py-3 text-left text-lg font-semibold text-gray-800">Price</th>
               <th className="px-6 py-3 text-left text-lg font-semibold text-gray-800">Quantity</th>
               <th className="px-6 py-3 text-left text-lg font-semibold text-gray-800">Subtotal</th>
+              <th className="px-20 py-3 text-left text-lg font-semibold text-gray-800">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {products.map((product: Product) => {
-              return (
-                <tr>
-                  <td className="border px-6 py-4">
-                    <div className="flex items-center justify-center">
-                      <img
-                        className="w-20 h-20 mr-40 rounded-lg fixed"
-                        src="https://img.freepik.com/photos-gratuite/equilibrage-du-smartphone-fond-rose_23-2150271746.jpg?size=338&ext=jpg&ga=GA1.1.1788068356.1706832000&semt=sph"
-                        alt="Product Image"
-                      />
-                      <div>
-                        <h3 className="text-xl font-semibold text-center">{product.name}</h3>
-                        <p className="text-gray-600 text-sm mt-2 text-center">{product.description}</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="border px-6 py-4 text-lg">{product.initalprice} $</td>
-                  <td className="border px-6 py-4">
-                    <input
-                      className="block w-20 bg-white border border-gray-300 px-4 py-2 rounded-md text-lg focus:outline-none focus:border-blue-500"
-                      type="number"
-                      defaultValue="1"
+            {products.map((product: Product, index: number) => (
+              <tr key={product.idproduct}>
+                <td className="border px-6 py-4">
+                  <div className="flex items-center">
+                    <img
+                      className="w-20 h-20 mr-4 rounded-lg"
+                      src={product.imgproducts[0].image}
+                      alt="Product Image"
                     />
-                  </td>
-                  <td className="border px-6 py-4 text-lg">$40.00</td>
-                </tr>
-              );
-            })}
+                    <div>
+                      <h3 className="text-xl font-semibold">{product.name}</h3>
+                      <p className="text-gray-600 text-sm mt-2">{product.description}</p>
+                    </div>
+                  </div>
+                </td>
+                <td className="border px-6 py-4 text-lg">{product.initalprice} $</td>
+                <td className="border px-6 py-4">
+                  <input
+                    className="block w-20 bg-white border border-gray-300 px-4 py-2 rounded-md text-lg focus:outline-none focus:border-blue-500"
+                    type="number"
+                    defaultValue="1"
+                    value={product.quantity}
+                    onChange={(e) => {
+                      const newQuantity = parseInt(e.target.value);
+                      setProducts((prevProducts) => {
+                        const updatedProducts = [...prevProducts];
+                        updatedProducts[index].quantity = newQuantity;
+                        return updatedProducts;
+                      });
+                    }}
+                  />
+                </td>
+                <td className="border px-6 py-4 text-lg">{calculateSubtotal(product)} $</td>
+                <td className="border px-1 py-1 ">
+                  <button className="text-red-600 hover:text-red-800" onClick={() => handleDelete(product.idproduct)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-12 w-40"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -82,7 +111,9 @@ export default function Cart() {
         <h3 className="text-xl font-bold mb-2 text-center">Cart Total</h3>
         <div className="flex justify-between">
           <span className="text-lg font-semibold">Subtotal:</span>
-          <span className="text-lg font-semibold">$70.00</span>
+          <span className="text-lg font-semibold">
+            {products.reduce((acc, product) => acc + calculateSubtotal(product), 0)} $
+          </span>
         </div>
         <div className="flex justify-between mt-2">
           <span className="text-lg font-semibold">Shipping:</span>
@@ -90,7 +121,9 @@ export default function Cart() {
         </div>
         <div className="flex justify-between mt-2">
           <span className="text-lg font-semibold">Total:</span>
-          <span className="text-lg font-semibold">$100.00</span>
+          <span className="text-lg font-semibold">
+            {products.reduce((acc, product) => acc + calculateSubtotal(product), 0)} $
+          </span>
         </div>
         <div className="mt-4 flex justify-center">
           <button className="bg-[#8D7B68] text-white px-14 py-3 rounded-md text-lg hover:bg-[#A4907C] focus:outline-none focus:bg-[#A4907C]">
